197612971ef72680d38881032c55f22a0b746ebf
Himanshu Tripathi <65823429+hackhim18@users.noreply.github.com>
2021-03-06 13:23:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+
+
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
